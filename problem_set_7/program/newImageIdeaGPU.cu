1b14ec3ba783a5e8aaa4be3979816e34a0b829c9
Per Magnus Veierland <pveierland@gmail.com>
2015-11-06 18:42:27 +0100

    PS7: CUDA version WIP

--- problem_set_7/program/newImageIdeaGPU.cu
+++ problem_set_7/program/newImageIdeaGPU.cu
@@ -1,8 +1,33 @@
+#include <stdexcept>
+#include <string>
+#include <iostream>
+
+#include <stdint.h>
+
 #include <math.h>
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include "ppmCU.h"
+
+#define cuda_call(f, ...) \
+    cuda_assert(f(__VA_ARGS__), __FILE__, __LINE__, #f)
+
+#define cuda_launch(kernel, grid_dim, block_dim, ...) \
+{ \
+    kernel<<<grid_dim, block_dim>>>(__VA_ARGS__); \
+    cuda_assert(cudaPeekAtLastError(), __FILE__, __LINE__, "kernel " #kernel " launch"); \
+}
+
+inline
+void
+cuda_assert(cudaError_t error, const char* file, const int line, const char* function)
+{
+    if (error)
+    {
+        throw std::runtime_error(std::string(function) + " failed: " + cudaGetErrorString(error));
+    }
+}
 
 // Image from:
 // http://7-themes.com/6971875-funny-flowers-pictures.html
@@ -13,6 +38,49 @@
 
 // TODO: You should implement this
 //__global__ void performNewIdeaFinalizationGPU( ... ) { ... }
+
+__device__ inline
+unsigned char
+computeFinalPixelValue(float value)
+{
+    if (value <= -1.0f)
+    {
+        value += 257.0f;
+    }
+    return (unsigned char)(floorf(min(max(value, 0.0f), 255.0f)));
+}
+
+// Image must have width divisible by 8 pixels
+__global__
+void
+performNewIdeaFinalizationGPU(
+    const int            image_width,
+    float* const         image_input_small,
+    float* const         image_input_large,
+    std::uint64_t* const image_output)
+{
+    const int offset = blockIdx.x * image_width * 24 + 24 * threadIdx.x;
+
+    // Input:  24 * float values = 24 * 4 bytes = 96 bytes (8 bytes * 12)
+    // Output: 24 bytes (8 bytes * 3)
+    for (int i = 0; i < 24; ++i)
+    {
+        image_output[offset + i] = computeFinalPixelValue(
+            image_input_large[offset + i] - image_input_small[offset + i]);
+    }
+
+    const int output_offset = blockIdx.x * image_width + threadIdx.x;
+
+    image_output[offset + 0] = (computeFinalPixelValue(image_input_large[offset + 0] - image_input_small[offset + 0]) << 56) |
+                               (computeFinalPixelValue(image_input_large[offset + 1] - image_input_small[offset + 1]) << 48) |
+                               (computeFinalPixelValue(image_input_large[offset + 2] - image_input_small[offset + 2]) << 40) |
+                               (computeFinalPixelValue(image_input_large[offset + 3] - image_input_small[offset + 3]) << 32) |
+                               (computeFinalPixelValue(image_input_large[offset + 4] - image_input_small[offset + 4]) << 24) |
+                               (computeFinalPixelValue(image_input_large[offset + 5] - image_input_small[offset + 5]) << 16) |
+                               (computeFinalPixelValue(image_input_large[offset + 6] - image_input_small[offset + 6]) <<  8) |
+                               (computeFinalPixelValue(image_input_large[offset + 7] - image_input_small[offset + 7]) <<  0);
+
+}
 
 // TODO: You should implement this
 //__global__ void convertImageToNewFormatGPU( ... ) { ... }
@@ -29,7 +97,7 @@ typedef struct {
      AccuratePixel *data;
 } AccurateImage;
 
-// Convert a PPM image to a high-precision format 
+// Convert a PPM image to a high-precision format
 AccurateImage *convertImageToNewFormat(PPMImage *image) {
 	// Make a copy
 	AccurateImage *imageAccurate;
@@ -42,7 +110,7 @@ AccurateImage *convertImageToNewFormat(PPMImage *image) {
 	}
 	imageAccurate->x = image->x;
 	imageAccurate->y = image->y;
-	
+
 	return imageAccurate;
 }
 
@@ -59,7 +127,7 @@ PPMImage *convertNewFormatToPPM(AccurateImage *image) {
 	}
 	imagePPM->x = image->x;
 	imagePPM->y = image->y;
-	
+
 	return imagePPM;
 }
 
@@ -69,7 +137,7 @@ AccurateImage *createEmptyImage(PPMImage *image){
 	imageAccurate->data = (AccuratePixel*)malloc(image->x * image->y * sizeof(AccuratePixel));
 	imageAccurate->x = image->x;
 	imageAccurate->y = image->y;
-	
+
 	return imageAccurate;
 }
 
@@ -80,23 +148,23 @@ void freeImage(AccurateImage *image){
 }
 
 void performNewIdeaIteration(AccurateImage *imageOut, AccurateImage *imageIn, int size) {
-	
+
 	// Iterate over each pixel
 	for(int senterX = 0; senterX < imageIn->x; senterX++) {
-	
+
 		for(int senterY = 0; senterY < imageIn->y; senterY++) {
-			
+
 			// For each pixel we compute the magic number
 			float sumR = 0;
 			float sumG = 0;
 			float sumB = 0;
 			int countIncluded = 0;
 			for(int x = -size; x <= size; x++) {
-			
+
 				for(int y = -size; y <= size; y++) {
 					int currentX = senterX + x;
 					int currentY = senterY + y;
-					
+
 					// Check if we are outside the bounds
 					if(currentX < 0)
 						continue;
@@ -106,25 +174,25 @@ void performNewIdeaIteration(AccurateImage *imageOut, AccurateImage *imageIn, in
 						continue;
 					if(currentY >= imageIn->y)
 						continue;
-					
+
 					// Now we can begin
-					int numberOfValuesInEachRow = imageIn->x; 
+					int numberOfValuesInEachRow = imageIn->x;
 					int offsetOfThePixel = (numberOfValuesInEachRow * currentY + currentX);
 					sumR += imageIn->data[offsetOfThePixel].red;
 					sumG += imageIn->data[offsetOfThePixel].green;
 					sumB += imageIn->data[offsetOfThePixel].blue;
-				
+
 					// Keep track of how many values we have included
 					countIncluded++;
 				}
-			
+
 			}
-			
+
 			// Now we compute the final value for all colours
 			float valueR = sumR / countIncluded;
 			float valueG = sumG / countIncluded;
 			float valueB = sumB / countIncluded;
-			
+
 			// Update the output image
 			int numberOfValuesInEachRow = imageOut->x; // R, G and B
 			int offsetOfThePixel = (numberOfValuesInEachRow * senterY + senterX);
@@ -135,79 +203,119 @@ void performNewIdeaIteration(AccurateImage *imageOut, AccurateImage *imageIn, in
 	}
 }
 
-// Perform the final step, and save it as a ppm in imageOut
-void performNewIdeaFinalization(AccurateImage *imageInSmall, AccurateImage *imageInLarge, PPMImage *imageOut) {
+//inline int
+//computeFinalPixelValue(float value)
+//{
+//    if (value <= -1.0f)
+//    {
+//        value += 257.0f;
+//    }
+//    return (int)(floorf(min(max(value, 0.0f), 255.0f)));
+//}
+//
+//void performNewIdeaFinalization(AccurateImage *imageInSmall, AccurateImage *imageInLarge, PPMImage *imageOut) {
+//
+//
+//	imageOut->x = imageInSmall->x;
+//	imageOut->y = imageInSmall->y;
+//
+//	for(int i = 0; i < imageInSmall->x * imageInSmall->y; i++) {
+//        imageOut->data[i].red   = computeFinalPixelValue(imageInLarge->data[i].red - imageInSmall->data[i].red);
+//        imageOut->data[i].green = computeFinalPixelValue(imageInLarge->data[i].green - imageInSmall->data[i].green);
+//        imageOut->data[i].blue  = computeFinalPixelValue(imageInLarge->data[i].blue - imageInSmall->data[i].blue);
+//	}
+//}
 
-	
-	imageOut->x = imageInSmall->x;
-	imageOut->y = imageInSmall->y;
-	
-	for(int i = 0; i < imageInSmall->x * imageInSmall->y; i++) {
-		float value = (imageInLarge->data[i].red - imageInSmall->data[i].red);
-		if(value > 255.0f)
-			imageOut->data[i].red = 255;
-		else if (value < -1.0f) {
-			value = 257.0f+value;
-			if(value > 255.0f)
-				imageOut->data[i].red = 255;
-			else
-				imageOut->data[i].red = floorf(value);
-		} else if (value > -1.0f && value < 0.0f) {
-			imageOut->data[i].red = 0;
-		} else {
-			imageOut->data[i].red = floorf(value);
-		}
-		
-		value = (imageInLarge->data[i].green - imageInSmall->data[i].green);
-		if(value > 255.0f)
-			imageOut->data[i].green = 255;
-		else if (value < -1.0f) {
-			value = 257.0f+value;
-			if(value > 255.0f)
-				imageOut->data[i].green = 255;
-			else
-				imageOut->data[i].green = floorf(value);
-		} else if (value > -1.0f && value < 0.0f) {
-			imageOut->data[i].green = 0;
-		} else {
-			imageOut->data[i].green = floorf(value);
-		}
-		
-		value = (imageInLarge->data[i].blue - imageInSmall->data[i].blue);
-		if(value > 255.0f)
-			imageOut->data[i].blue = 255;
-		else if (value < -1.0f) {
-			value = 257.0f+value;
-			if(value > 255.0f)
-				imageOut->data[i].blue = 255;
-			else
-				imageOut->data[i].blue = floorf(value);
-		} else if (value > -1.0f && value < 0.0f) {
-			imageOut->data[i].blue = 0;
-		} else {
-			imageOut->data[i].blue = floorf(value);
-		}
-	}
-}
+//// Perform the final step, and save it as a ppm in imageOut
+//void performNewIdeaFinalization(AccurateImage *imageInSmall, AccurateImage *imageInLarge, PPMImage *imageOut) {
+//
+//
+//	imageOut->x = imageInSmall->x;
+//	imageOut->y = imageInSmall->y;
+//
+//	for(int i = 0; i < imageInSmall->x * imageInSmall->y; i++) {
+//		float value = (imageInLarge->data[i].red - imageInSmall->data[i].red);
+//		if(value > 255.0f)
+//			imageOut->data[i].red = 255;
+//		else if (value < -1.0f) {
+//			value = 257.0f+value;
+//			if(value > 255.0f)
+//				imageOut->data[i].red = 255;
+//			else
+//				imageOut->data[i].red = floorf(value);
+//		} else if (value > -1.0f && value < 0.0f) {
+//			imageOut->data[i].red = 0;
+//		} else {
+//			imageOut->data[i].red = floorf(value);
+//		}
+//
+//		value = (imageInLarge->data[i].green - imageInSmall->data[i].green);
+//		if(value > 255.0f)
+//			imageOut->data[i].green = 255;
+//		else if (value < -1.0f) {
+//			value = 257.0f+value;
+//			if(value > 255.0f)
+//				imageOut->data[i].green = 255;
+//			else
+//				imageOut->data[i].green = floorf(value);
+//		} else if (value > -1.0f && value < 0.0f) {
+//			imageOut->data[i].green = 0;
+//		} else {
+//			imageOut->data[i].green = floorf(value);
+//		}
+//
+//		value = (imageInLarge->data[i].blue - imageInSmall->data[i].blue);
+//		if(value > 255.0f)
+//			imageOut->data[i].blue = 255;
+//		else if (value < -1.0f) {
+//			value = 257.0f+value;
+//			if(value > 255.0f)
+//				imageOut->data[i].blue = 255;
+//			else
+//				imageOut->data[i].blue = floorf(value);
+//		} else if (value > -1.0f && value < 0.0f) {
+//			imageOut->data[i].blue = 0;
+//		} else {
+//			imageOut->data[i].blue = floorf(value);
+//		}
+//	}
+//}
 
+// 1920 x 1200
 int main(int argc, char** argv) {
-	
 	PPMImage *image;
-        
+
 	if(argc > 1) {
 		image = readPPM("flower.ppm");
 	} else {
 		image = readStreamPPM(stdin);
 	}
+    
+    std::cout << "P" << std::endl;
+
+    const int pixels_in_image = image->x * image->y;
+    const int bytes_per_pixel = 3 * sizeof(float);
+
+    float*         device_image_a      = NULL;
+    float*         device_image_b      = NULL;
+    unsigned char* device_image_output = NULL;
+
+    cuda_call(cudaMalloc, &device_image_a, pixels_in_image * bytes_per_pixel);
+    cuda_call(cudaMalloc, &device_image_b, pixels_in_image * bytes_per_pixel);
+    cuda_call(cudaMalloc, &device_image_output, pixels_in_image);
 
 	AccurateImage *imageUnchanged = convertImageToNewFormat(image); // save the unchanged image from input image
 	AccurateImage *imageBuffer = createEmptyImage(image);
 	AccurateImage *imageSmall = createEmptyImage(image);
 	AccurateImage *imageBig = createEmptyImage(image);
-	
+
 	PPMImage *imageOut;
 	imageOut = (PPMImage *)malloc(sizeof(PPMImage));
+    imageOut->x = image->x;
+    imageOut->y = image->y;
 	imageOut->data = (PPMPixel*)malloc(image->x * image->y * sizeof(PPMPixel));
+
+    std::cout << "E" << std::endl;
 
 	// Process the tiny case:
 	performNewIdeaIteration(imageSmall, imageUnchanged, 2);
@@ -215,53 +323,107 @@ int main(int argc, char** argv) {
 	performNewIdeaIteration(imageSmall, imageBuffer, 2);
 	performNewIdeaIteration(imageBuffer, imageSmall, 2);
 	performNewIdeaIteration(imageSmall, imageBuffer, 2);
-	
+
 	// Process the small case:
 	performNewIdeaIteration(imageBig, imageUnchanged,3);
 	performNewIdeaIteration(imageBuffer, imageBig,3);
 	performNewIdeaIteration(imageBig, imageBuffer,3);
 	performNewIdeaIteration(imageBuffer, imageBig,3);
 	performNewIdeaIteration(imageBig, imageBuffer,3);
-	
-	// save tiny case result
-	performNewIdeaFinalization(imageSmall,  imageBig, imageOut);
+    
+    std::cout << "N " << device_image_a << std::endl;
+
+    cuda_call(cudaMemcpy, device_image_a, imageSmall->data,
+              pixels_in_image * bytes_per_pixel, cudaMemcpyHostToDevice);
+    
+    std::cout << "X" << std::endl;
+
+    cuda_call(cudaMemcpy, device_image_b, imageBig->data,
+              pixels_in_image * bytes_per_pixel, cudaMemcpyHostToDevice);
+    
+    std::cout << "Y" << std::endl;
+
+    cuda_launch(performNewIdeaFinalizationGPU, 200, 240,
+                1920, device_image_a, device_image_b, device_image_output);
+    
+    std::cout << "Z" << std::endl;
+
+    cuda_call(cudaMemcpy,
+              imageOut->data,
+              device_image_output,
+              pixels_in_image,
+              cudaMemcpyDeviceToHost);
+    
+    std::cout << "1" << std::endl;
+
 	if(argc > 1) {
 		writePPM("flower_tiny.ppm", imageOut);
 	} else {
 		writeStreamPPM(stdout, imageOut);
 	}
 
-	
-	// Process the medium case:
-	performNewIdeaIteration(imageSmall, imageUnchanged, 5);
-	performNewIdeaIteration(imageBuffer, imageSmall, 5);
-	performNewIdeaIteration(imageSmall, imageBuffer, 5);
-	performNewIdeaIteration(imageBuffer, imageSmall, 5);
-	performNewIdeaIteration(imageSmall, imageBuffer, 5);
-	
-	// save small case
-	performNewIdeaFinalization(imageBig,  imageSmall,imageOut);
-	if(argc > 1) {
-		writePPM("flower_small.ppm", imageOut);
-	} else {
-		writeStreamPPM(stdout, imageOut);
-	}
+//	// Process the medium case:
+//	performNewIdeaIteration(imageSmall, imageUnchanged, 5);
+//	performNewIdeaIteration(imageBuffer, imageSmall, 5);
+//	performNewIdeaIteration(imageSmall, imageBuffer, 5);
+//	performNewIdeaIteration(imageBuffer, imageSmall, 5);
+//	performNewIdeaIteration(imageSmall, imageBuffer, 5);
+//
+//	// save small case
+//    cuda_call(cudaMemcpy, device_image_a, imageBig->data,
+//              pixels_in_image * bytes_per_pixel, cudaMemcpyHostToDevice);
+//
+//    cuda_call(cudaMemcpy, device_image_b, imageSmall->data,
+//              pixels_in_image * bytes_per_pixel, cudaMemcpyHostToDevice);
+//
+//    cuda_launch(performNewIdeaFinalizationGPU, 1200, 240,
+//                1920, device_image_a, device_image_b, device_image_output);
+//
+//    cuda_call(cudaMemcpy,
+//              imageOut->data,
+//              device_image_output,
+//              pixels_in_image,
+//              cudaMemcpyDeviceToHost);
+//	if(argc > 1) {
+//		writePPM("flower_small.ppm", imageOut);
+//	} else {
+//		writeStreamPPM(stdout, imageOut);
+//	}
+//    
+//    std::cout << "5" << std::endl;
+//
+//	// process the large case
+//	performNewIdeaIteration(imageBig, imageUnchanged, 8);
+//	performNewIdeaIteration(imageBuffer, imageBig, 8);
+//	performNewIdeaIteration(imageBig, imageBuffer, 8);
+//	performNewIdeaIteration(imageBuffer, imageBig, 8);
+//	performNewIdeaIteration(imageBig, imageBuffer, 8);
+//
+//    cuda_call(cudaMemcpy, device_image_a, imageSmall->data,
+//              pixels_in_image * bytes_per_pixel, cudaMemcpyHostToDevice);
+//
+//    cuda_call(cudaMemcpy, device_image_b, imageBig->data,
+//              pixels_in_image * bytes_per_pixel, cudaMemcpyHostToDevice);
+//
+//    cuda_launch(performNewIdeaFinalizationGPU, 1200, 240,
+//                1920, device_image_a, device_image_b, device_image_output);
+//
+//    cuda_call(cudaMemcpy,
+//              imageOut->data,
+//              device_image_output,
+//              pixels_in_image,
+//              cudaMemcpyDeviceToHost);
+//
+//	if(argc > 1) {
+//		writePPM("flower_medium.ppm", imageOut);
+//	} else {
+//		writeStreamPPM(stdout, imageOut);
+//	}
 
-	// process the large case
-	performNewIdeaIteration(imageBig, imageUnchanged, 8);
-	performNewIdeaIteration(imageBuffer, imageBig, 8);
-	performNewIdeaIteration(imageBig, imageBuffer, 8);
-	performNewIdeaIteration(imageBuffer, imageBig, 8);
-	performNewIdeaIteration(imageBig, imageBuffer, 8);
+    cuda_call(cudaFree, device_image_a);
+    cuda_call(cudaFree, device_image_b);
+    cuda_call(cudaFree, device_image_output);
 
-	// save the medium case
-	performNewIdeaFinalization(imageSmall,  imageBig, imageOut);
-	if(argc > 1) {
-		writePPM("flower_medium.ppm", imageOut);
-	} else {
-		writeStreamPPM(stdout, imageOut);
-	}
-	
 	// free all memory structures
 	freeImage(imageUnchanged);
 	freeImage(imageBuffer);
@@ -271,7 +433,7 @@ int main(int argc, char** argv) {
 	free(imageOut);
 	free(image->data);
 	free(image);
-	
+
 	return 0;
 }
 
